baaa31c065d18d337f2eaa1eda0ad61acc99604a
baubry <baptiste.aubry@arturia.com>
2015-10-29 13:55:42 +0100

    automatic discovery of visual studio installation directory

--- cmake/findVisualStudio.cmake
+++ cmake/findVisualStudio.cmake
@@ -1,6 +1,19 @@
 cmake_minimum_required(VERSION 2.8)
 
-set(MSVC_UNIT_TEST_DIR "C:/Program Files (x86)/Microsoft Visual Studio 12.0/VC/UnitTest/")#TODO make it discoverable
+set(CUTI_VS_PATH_PREFIX "C:/Program Files (x86)/Microsoft Visual Studio ")
+set(CUTI_VS_PATH_SUFIX "/VC/UnitTest/")
+
+if(EXISTS ${CUTI_VS_PATH_PREFIX}14.0${CUTI_VS_PATH_SUFIX})
+	set(CUTI_VS_VERSION 14.0)
+elseif(EXISTS ${CUTI_VS_PATH_PREFIX}12.0${CUTI_VS_PATH_SUFIX})
+	set(CUTI_VS_VERSION 12.0)
+elseif(EXISTS ${CUTI_VS_PATH_PREFIX}11.0${CUTI_VS_PATH_SUFIX})
+	set(CUTI_VS_VERSION 11.0)
+else()
+	message(FATAL_ERROR "CUTI : Visual studio install directory not found or unsupported version")
+endif()
+
+set(MSVC_UNIT_TEST_DIR ${CUTI_VS_PATH_PREFIX}${CUTI_VS_VERSION}${CUTI_VS_PATH_SUFIX})
 
 include_directories(${MSVC_UNIT_TEST_DIR}/include)
 link_directories(${MSVC_UNIT_TEST_DIR}lib/)
