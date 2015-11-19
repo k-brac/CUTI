bb1eea92c931685319f47c4a1a5f405e438f6e41
Baptiste Aubry <baptiste.aubry@arturia.com>
2015-11-19 12:35:09 +0100

    added automatic support of Xcode (no more manual settings)

--- cmake/findCUTI.cmake
+++ cmake/findCUTI.cmake
@@ -3,6 +3,7 @@ cmake_minimum_required(VERSION 2.8)
 set(CUTI_TOP_DIR ./)
 
 set(CUTI_INCLUDE_DIRS ${CUTI_TOP_DIR}include)
+
 if(CUTI_USES_MSVC_UNIT_BACKEND)
 	if(WIN32)
 		find_package(VisualStudio REQUIRED)
@@ -24,3 +25,27 @@ endif(CUTI_USES_MSVC_UNIT_BACKEND)
 
 include_directories(${CUTI_INCLUDE_DIRS})
 get_filename_component(CUTI_INCLUDE ${CUTI_TOP_DIR}include/Cuti.h ABSOLUTE)
+IF(APPLE)
+	get_filename_component(CUTI_SOURCE ${CUTI_TOP_DIR}src/Cuti.mm ABSOLUTE)
+	set_source_files_properties(${CUTI_SOURCE} PROPERTIES COMPILE_FLAGS "-x objective-c++")
+ENDIF(APPLE)
+
+function(cuti_creates_test_target target testee)
+
+	IF(APPLE)
+		find_package(XCTest REQUIRED)
+		set(MACOSX_BUNDLE_GUI_IDENTIFIER "cuti.bundle.identifier")
+		set_target_properties(${testee} PROPERTIES
+			FRAMEWORK TRUE
+			VERSION "1.0.0"
+			SOVERSION "1.0.0"
+			FRAMEWORK_VERSION "A"
+	)
+		xctest_add_bundle(${target} ${testee} ${CUTI_SOURCE} ${ARGN} ${CUTI_INCLUDE})
+	ELSE()
+		add_library(${target} SHARED ${ARGN} ${CUTI_INCLUDE})
+	ENDIF(APPLE)
+
+	target_link_libraries(${target} PRIVATE debug ${CUTI_UNIT_TEST_LIBRARIES_DEBUG} ${testee})
+	target_link_libraries(${target} PRIVATE optimized ${CUTI_UNIT_TEST_LIBRARIES_RELEASE} ${testee})
+endfunction(cuti_creates_test_target)
