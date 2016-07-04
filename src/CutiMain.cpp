100e53ced84dc14a10d7952f29e0310df6a0d0d7
k-brac <baptiste.h.aubry@gmail.com>
2016-07-04 11:41:31 +0200

    licensing

--- src/CutiMain.cpp
+++ src/CutiMain.cpp
@@ -1,5 +1,35 @@
+/*The MIT License (MIT)
+
+Copyright (c) 2015 k-brac
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "cppunit/extensions/TestFactoryRegistry.h"
+#ifdef WIN32
+#pragma warning( push )
+#pragma warning( disable : 4251 )
+#endif
 #include "cppunit/CompilerOutputter.h"
+#ifdef WIN32
+#pragma warning( pop )
+#endif
 #include "cppunit/TestResult.h"
 #include "cppunit/TestResultCollector.h"
 #include "cppunit/TestRunner.h"
@@ -9,13 +39,7 @@
 
 bool runPlugin(const std::string &plugPath, const std::vector<std::string> &testPaths) {
 	CppUnit::PlugInManager pluginManager;
-	try {
-		pluginManager.load(plugPath);
-	}
-	catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
-		return false;
-	}
+	pluginManager.load(plugPath);
 
 	// Create the event manager and test controller
 	CppUnit::TestResult controller;
