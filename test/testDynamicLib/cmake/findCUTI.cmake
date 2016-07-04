5b209e2bf1725505b79a0b557efca6bc11f8bf0a
k-brac <baptiste.h.aubry@gmail.com>
2016-07-04 11:40:04 +0200

    remove useless files

--- test/testDynamicLib/cmake/findCUTI.cmake
+++
@@ -1,5 +0,0 @@
-cmake_minimum_required (VERSION 3.2)
-
-set(CUTI_TOP_DIR ../../../Cuti/)
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/${CUTI_TOP_DIR}/cmake/")
-include(${CUTI_TOP_DIR}/cmake/Cuti.cmake)
