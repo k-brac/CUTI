f3ddd072a226c1a5bc090c97d97aed7080ca2082
k-brac <baptiste.h.aubry@gmail.com>
2017-03-13 13:52:11 +0100

    add template test file

---
+++ test/TestTemplate.cpp
@@ -0,0 +1,62 @@
+/*The MIT License (MIT)
+
+ Copyright (c) 2017 k-brac
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+/**
+* Mandatory include. Should always be on top
+*/
+#include "Cuti.h"
+
+/**
+* Declare a test class allowing for standalone or integrated test runner
+*/
+CUTI_TEST_CLASS(TestTemplate) {
+private:
+    bool val = false;
+public:
+    /**
+     * Test suit declaration and test case registration
+     */
+    CPPUNIT_TEST_SUITE(TestTemplate);
+    CPPUNIT_TEST(testSimpleAssert);
+    CPPUNIT_TEST_SUITE_END();
+
+public://Mandatory because CPPUNIT_TEST_SUITE_END can expend to private:
+    /**
+    * Optional. Executed before every test case
+    */
+    CUTI_SET_UP() {
+        val = true;
+    }
+
+    /**
+    * Optional. Executed after every test case
+    */
+    CUTI_TEAR_DOWN() {
+        val = false;
+    }
+
+    void testSimpleAssert() {
+        CPPUNIT_ASSERT(val);
+    }
+};
+CPPUNIT_TEST_SUITE_REGISTRATION(TestTemplate);
