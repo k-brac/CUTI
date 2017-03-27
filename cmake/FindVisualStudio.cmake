9b4e4d6005e675c89c76ba2d0b312e6e4da4839e
k-brac <baptiste.h.aubry@gmail.com>
2017-03-27 12:30:06 +0200

    cleanup and comments

--- cmake/FindVisualStudio.cmake
+++ cmake/FindVisualStudio.cmake
@@ -22,6 +22,8 @@
 
 cmake_minimum_required(VERSION 2.8)
 
+#Looks for the install directory of the currently used visual studio
+
 set(CUTI_VS_PATH_PREFIX "C:/Program Files (x86)/Microsoft Visual Studio ")
 set(CUTI_VS_PATH_SUFIX "VC/UnitTest")
 
